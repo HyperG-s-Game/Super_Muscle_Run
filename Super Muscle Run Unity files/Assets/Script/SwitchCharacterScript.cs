f3453ed5ac841359a2cfd166a3d3cb51f330aaaa
yogeshParihar2105 <yogesh.p@playresume.in>
2021-12-29 13:27:14 +0530

    The Player are switching correctly with the models

--- Super Muscle Run Unity files/Assets/Script/SwitchCharacterScript.cs
+++ Super Muscle Run Unity files/Assets/Script/SwitchCharacterScript.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class SwitchCharacterScript : MonoBehaviour {
 
 	// referenses to controlled game objects
-	public GameObject avatar1, avatar2, avatar3, avatar4, avatar5;
+	public GameObject avatar1, avatar2, avatar3, avatar4, avatar5, avatar6, avatar7, avatar8, avatar9, avatar10;
 
 	// variable contains which avatar is on and active
-	int whichAvatarIsOn = 3;
+	int whichAvatarIsOn = 6;
 
 	// Use this for initialization
 	void Start () {
 
 		avatar1.gameObject.SetActive (false);
 		avatar2.gameObject.SetActive (false);
-		avatar3.gameObject.SetActive(true);
+		avatar3.gameObject.SetActive(false);
 		avatar4.gameObject.SetActive(false);
 		avatar5.gameObject.SetActive(false);
+		avatar6.gameObject.SetActive(true);
+		avatar7.gameObject.SetActive(false);
+		avatar8.gameObject.SetActive(false);
+		avatar9.gameObject.SetActive(false);
+		avatar10.gameObject.SetActive(false);
 	}
 
+	//If player consume fat
 	public void Fat()
     {
 		if (whichAvatarIsOn == 2)
@@ -30,74 +36,256 @@ public class SwitchCharacterScript : MonoBehaviour {
 			avatar3.gameObject.SetActive(false);
 			avatar4.gameObject.SetActive(false);
 			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
 		}
 		else if (whichAvatarIsOn == 3)
         {
 			whichAvatarIsOn = 2;
-			avatar2.gameObject.SetActive(true);
 			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(true);
 			avatar3.gameObject.SetActive(false);
 			avatar4.gameObject.SetActive(false);
 			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
 		}
 		else if (whichAvatarIsOn == 4)
 		{
-			whichAvatarIsOn = 2;
-			avatar2.gameObject.SetActive(true);
+			whichAvatarIsOn = 3;
 			avatar1.gameObject.SetActive(false);
-			avatar3.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(true);
 			avatar4.gameObject.SetActive(false);
 			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
 		}
 		else if (whichAvatarIsOn == 5)
 		{
-			whichAvatarIsOn = 2;
-			avatar2.gameObject.SetActive(true);
+			whichAvatarIsOn = 4;
 			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(true);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 6)
+		{
+			whichAvatarIsOn = 1;
+			avatar1.gameObject.SetActive(true);
+			avatar2.gameObject.SetActive(false);
 			avatar3.gameObject.SetActive(false);
 			avatar4.gameObject.SetActive(false);
 			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 7)
+		{
+			whichAvatarIsOn = 2;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(true);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 8)
+		{
+			whichAvatarIsOn = 3;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(true);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 9)
+		{
+			whichAvatarIsOn = 4;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(true);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 10)
+		{
+			whichAvatarIsOn = 5;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(true);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
 		}
 
 	}
+
+
+	//If Player consume Muscle
 	public void Muscle()
 	{
-		if (whichAvatarIsOn == 1)
+		if (whichAvatarIsOn == 9)
 		{
-			whichAvatarIsOn = 2;
-			avatar2.gameObject.SetActive(true);
-			avatar1.gameObject.SetActive(false);
-			avatar3.gameObject.SetActive(false);
-			avatar4.gameObject.SetActive(false);
-			avatar5.gameObject.SetActive(false);
-		}
-		else if (whichAvatarIsOn == 2)
-		{
-			whichAvatarIsOn = 3;
-			avatar3.gameObject.SetActive(true);
-			avatar1.gameObject.SetActive(false);
-			avatar2.gameObject.SetActive(false);
-			avatar4.gameObject.SetActive(false);
-			avatar5.gameObject.SetActive(false);
-		}
-		else if (whichAvatarIsOn == 3)
-		{
-			whichAvatarIsOn = 4;
-			avatar4.gameObject.SetActive(true);
+			whichAvatarIsOn = 10;
 			avatar1.gameObject.SetActive(false);
 			avatar2.gameObject.SetActive(false);
 			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
 			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(true);
+		}
+		else if (whichAvatarIsOn == 8)
+		{
+			whichAvatarIsOn = 9;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(true);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 7)
+		{
+			whichAvatarIsOn = 8;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(true);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 6)
+		{
+			whichAvatarIsOn = 7;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(true);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 5)
+		{
+			whichAvatarIsOn = 10;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(true);
 		}
 		else if (whichAvatarIsOn == 4)
 		{
-			whichAvatarIsOn = 5;
-			avatar5.gameObject.SetActive(true);
+			whichAvatarIsOn = 9;
 			avatar1.gameObject.SetActive(false);
 			avatar2.gameObject.SetActive(false);
 			avatar3.gameObject.SetActive(false);
 			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(true);
+			avatar10.gameObject.SetActive(false);
 		}
-
+		else if (whichAvatarIsOn == 3)
+		{
+			whichAvatarIsOn = 8;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(true);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 2)
+		{
+			whichAvatarIsOn = 7;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(false);
+			avatar7.gameObject.SetActive(true);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
+		else if (whichAvatarIsOn == 1)
+		{
+			whichAvatarIsOn = 6;
+			avatar1.gameObject.SetActive(false);
+			avatar2.gameObject.SetActive(false);
+			avatar3.gameObject.SetActive(false);
+			avatar4.gameObject.SetActive(false);
+			avatar5.gameObject.SetActive(false);
+			avatar6.gameObject.SetActive(true);
+			avatar7.gameObject.SetActive(false);
+			avatar8.gameObject.SetActive(false);
+			avatar9.gameObject.SetActive(false);
+			avatar10.gameObject.SetActive(false);
+		}
 	}
 }
